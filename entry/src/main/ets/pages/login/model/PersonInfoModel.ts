3dc72f864f431e0106866491f5f0945b2cbe1a66
jiafuyoung <jiafu_young@163.com>
2024-01-22 03:00:50 +0800

    修改注释

--- entry/src/main/ets/pages/login/model/PersonInfoModel.ts
+++ entry/src/main/ets/pages/login/model/PersonInfoModel.ts
@@ -1,5 +1,5 @@
 import "@ohos/axios"
-import axios from '@ohos/axios';
+import axios, { formToJSON } from '@ohos/axios';
 import "../../../constants/HttpConfig"
 import HttpConfig from '../../../constants/HttpConfig'
 import { PersonInfo } from './PersonInfo';
@@ -10,17 +10,18 @@ class PersonInfoModel {
   getPersonInfo(): Promise<PersonInfo> {
     return new Promise((resolve,reject) => {
       axios.get(
-        HttpConfig.baseUrl+"/login/cellphone?phone=15720601089&password=L159l296159",
+        // HttpConfig.baseUrl+"/login/cellphone?phone=phone&password=pass",
+        HttpConfig.baseUrl+"/playlist/catlist",
         {
           // params: {
-          //   phone: "15720601089", password: "L159l296159"
+          //   phone: "phone", password: "pass"
           // }
         }
       ).then(res => {
         if (res.status == 200) {
-          console.log("登录代理服务器成功")
-          console.log(res.data)
-          // resolve(JSON.parse(res.data))
+          console.log("登录代理服务器成功",JSON.stringify(res.data))
+          // console.log()
+          console.log("为什么不打印呢")
         } else {
           console.log("登录获取信息失败",JSON.stringify(res))
         }
